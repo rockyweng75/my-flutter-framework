202151a6a292a70238fd339e257ee7a15c3d7e96
rockyweng75 <rockyweng75@users.noreply.github.com>
2025-05-20 02:59:08 +0000

    deploy: 3ccdaf464dc00f0a9d1d1bd4daf2dc025bd88124

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "34beda9f39eb7d992d46125ca868dc61",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
-"flutter_bootstrap.js": "77d775c413f9cf29193e0bdbc1325b69",
+"flutter_bootstrap.js": "a7abc87f9c04c0c29650c36b1c800e15",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "edd8104e8e2f62b5c78a94134c05dec1",
 "/": "edd8104e8e2f62b5c78a94134c05dec1",
